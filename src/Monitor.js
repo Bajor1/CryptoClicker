866d9139ca00b26f998780234116e5f21a369619
WojciechBabik <faccomim@gmail.com>
2026-03-12 11:17:00 +0100

    monitor 2 commit

--- src/Monitor.js
+++ src/Monitor.js
@@ -5,29 +5,44 @@ function Monitor() {
   return (
     <div className="monitor">
       <img src="/monior.png" className="monitor-img" alt="monitor" />
+        <div className='screen'>
+          <div className="icon icon1">
+            <button>
+              <img src="/folderXP_icon.png" alt="miner" />
+              <h5>Miner.exe</h5>
+            </button>
+            
+          </div>
 
-      <div className="icon icon1">
-        <button>
-          <img src="/folderXP_icon.png" alt="miner" />
-          <h5>Miner.exe</h5>
-        </button>
-        
-      </div>
+          <div className="icon icon2">
+            <button>
+              <img src="/internetExplorer_icon.png" alt="internet" />
+              <h5>Internet</h5>
+            </button>
+          </div>
 
-      <div className="icon icon2">
-        <button>
-          <img src="/internetExplorer_icon.png" alt="google" />
-          <h5>Google</h5>
-        </button>
-      </div>
+          <div className="icon icon3">
+            <button>
+                <img src="/folderXP_icon.png" alt="marketWatch" />
+                <h5>MarketWatch.exe</h5>
+            </button>
+          </div>
 
-      <div className="icon icon3">
-        <button>
-            <img src="/folderXP_icon.png" alt="marketWatch" />
-            <h5>MarketWatch.exe</h5>
-        </button>
-      </div>
+          <div className="icon icon4">
+            <button>
+                <img src="/myComputer_icon.png" alt="MyComputer" />
+                <h5>My Computer</h5>
+            </button>
+          </div>
 
+          <div className="icon icon5">
+            <button>
+                <img src="/folderXP_icon.png" alt="Avergainer" />
+                <h5>Avergainer.exe</h5>
+            </button>
+          </div>
+          <div className="scanlines"></div>
+        </div>
     </div>
   )
 }
