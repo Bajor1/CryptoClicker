dbe5e15bf3c177f266f1931f5dec32b02940ad74
Bajor1 <majorbajor123@gmail.com>
2026-03-16 11:23:21 +0100

    FallingElementFINAL
    odkrecanie srubek, sprawdzanie czy srubki sa odkrecone + EASTEREGG minigra

--- src/Components.js
+++ src/Components.js
@@ -5,15 +5,47 @@ import FallingElement from './FallingElement.js'
 import { useState, useRef, useEffect } from "react";
 
 function Components() {
+  const [screwsUnscrewed, setScrewsUnscewed] = useState(0);
+  const [secretCounter, setSecretCounter] = useState(0);
 
+  function IncrementUnscrewedScrews()
+  {
+    setScrewsUnscewed(screwsUnscrewed + 1);
+  }
+
+  function IncrementSecretCounter()
+  {
+    setSecretCounter(secretCounter + 1);
+  }
   return (
     <div className="Components">
       <div className="case">
-        <FallingElement props={"caseCover"}/>
-        <FallingElement props={"screw"}/>
-        <FallingElement props={"screw"}/>
-        <FallingElement props={"screw"}/>
-        <FallingElement props={"screw"}/>
+        {screwsUnscrewed >=4 ? <FallingElement props={["caseCover", IncrementSecretCounter]}/> : <div className="caseCover" style={{ transform: `translateX(-15px) translateY(-20px)` }}></div>}
+        <FallingElement props={["screw", IncrementUnscrewedScrews]}/>
+        <FallingElement props={["screw", IncrementUnscrewedScrews]}/>
+        <FallingElement props={["screw", IncrementUnscrewedScrews]}/>
+        <FallingElement props={["screw", IncrementUnscrewedScrews]}/>
+      </div>
+
+
+      {//easter egg:
+      }
+      <div className='counterOutline' style={{
+    transform: secretCounter > 1 ? "translateY(0px)" : "translateY(-200px)"
+  }}>
+        <div className='counterInside'> 
+          <div className="glitchText">
+            {secretCounter < 10
+              ? "000" + secretCounter
+              : secretCounter < 100
+                ? "00" + secretCounter
+                : secretCounter < 1000
+                  ? "0" + secretCounter
+                  : secretCounter
+            }
+          </div>
+          <div className="counterOverlay"></div>
+        </div>
       </div>
     </div>
   );
