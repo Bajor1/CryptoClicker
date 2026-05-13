a21dfa5c3ffc920ecd88645a6bf6aaba016ce8e3
Bajor1 <majorbajor123@gmail.com>
2026-05-13 22:07:12 +0200

    sklkep

--- src/App.js
+++ src/App.js
@@ -484,10 +484,10 @@ function App() {
     const [minRandomGenValue, setMinRandomGenValue] = useState(0.6);
     const [maxRandomGenValue, setMaxRandomGenValue] = useState(1);
 
-    const [stashedGPUs, setStashedGPUs] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]);
-    const [stashedPowerSupplies, setStashedPowerSupplies] = useState([0,1,2,3,4,5,6,7,8,9,10,11]);
-    const [stashedCoolings, setStashedCoolings] = useState([0,1,2,3,4,5,6]);
-    const [stashedMotherboards, setStashedMotherboards] = useState([1,2,3,4,5,6,7]);
+    const [stashedGPUs, setStashedGPUs] = useState([]);
+    const [stashedPowerSupplies, setStashedPowerSupplies] = useState([]);
+    const [stashedCoolings, setStashedCoolings] = useState([]);
+    const [stashedMotherboards, setStashedMotherboards] = useState([]);
 
     const [income, setIncome] = useState(0);
 
@@ -690,10 +690,21 @@ function App() {
         changeComponent={changeComponent}
         
         ></Components> : 
-    /*currentUI == "Shop" (else statement)*/ <Shop  GPU={GPU}
-  motherboard={motherboard}
-  powerSupply={powerSupply}
-  cooling={cooling}/> }
+        /*currentUI == "Shop" (else statement)*/ <Shop  
+        GPU={GPU}
+        motherboard={motherboard}
+        powerSupply={powerSupply}
+        cooling={cooling}
+
+        setStashedGPUs={setStashedGPUs}
+        setStashedPowerSupplies={setStashedPowerSupplies}
+        setStashedMotherboards={setStashedMotherboards}
+        setStashedCoolings={setStashedCoolings}
+
+        stashedGPUs={stashedGPUs}
+        stashedPowerSupplies={stashedPowerSupplies}
+        stashedMotherboards={stashedMotherboards}
+        stashedCoolings={stashedCoolings} /> }
     </div>
   );
 }
--- src/Shop.js
+++ src/Shop.js
@@ -1,14 +1,264 @@
 import "./shop.css";
+import './Components.css';
 import bg from "./misc/sklep_bkg.png";
 import regal from "./misc/regal.png";
+import { useState, useEffect } from "react";
 
-export default function Shop() {
+export default function Shop(props) {
+
+  const [randomizedGPUs, setRandomizedGPUs] = useState([0,0,0,0,0,0]);
+  const [randomizedPowerSupplies, setRandomizedPowerSupplies] = useState([0, 0, 0, 0]);
+  const [randomizedMotherboards, setRandomizedMotherboards] = useState([0, 0]);
+  const [randomizedCoolings, setRandomizedCoolings] = useState([0, 0]);
+  
+  function randomizeShop()
+  {
+    setRandomizedGPUs([]);
+    for (let i=0; i<6; i++)
+    {
+      setRandomizedGPUs(prev => prev.concat(Math.floor(Math.random()*(props.GPU.length))));
+    }
+    setRandomizedPowerSupplies([]);
+    for (let i=0; i<4; i++)
+    {
+      setRandomizedPowerSupplies(prev => prev.concat(Math.floor(Math.random()*(props.powerSupply.length))));
+    }
+    setRandomizedMotherboards([]);
+    for (let i=0; i<2; i++)
+    {
+      setRandomizedMotherboards(prev => prev.concat(Math.floor(Math.random()*(props.motherboard.length))));
+    }
+    setRandomizedCoolings([]);
+    for (let i=0; i<2; i++)
+    {
+      setRandomizedCoolings(prev => prev.concat(Math.floor(Math.random()*(props.cooling.length))));
+    }
+  }
+
+  function buyComponent(type, componentID, cost)
+{
+  switch(type)
+  {
+    case "GPU":
+    {
+      props.setStashedGPUs(prev =>
+        [...prev, componentID]
+      );
+      setRandomizedGPUs(prev =>
+        prev.filter(id => id !== componentID)
+      );
+      break;
+    }
+    case "powerSupply":
+    {
+      props.setStashedPowerSupplies(prev =>
+        [...prev, componentID]
+      );
+      setRandomizedPowerSupplies(prev =>
+        prev.filter(id => id !== componentID)
+      );
+      break;
+    }
+    case "motherboard":
+    {
+      props.setStashedMotherboards(prev =>
+        [...prev, componentID]
+      );
+      setRandomizedMotherboards(prev =>
+        prev.filter(id => id !== componentID)
+      );
+      break;
+    }
+    case "cooling":
+    {
+      props.setStashedCoolings(prev =>
+        [...prev, componentID]
+      );
+      setRandomizedCoolings(prev =>
+        prev.filter(id => id !== componentID)
+      );
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+  useEffect(() => {
+  
+    randomizeShop();
+  
+    const interval = setInterval(() => {
+      randomizeShop();
+    }, 300000);
+  
+    return () => clearInterval(interval);
+  
+  }, []);
+
   return (
     <div
       className="shop"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      <img src={regal} alt="shelf" className="shelf" />
+
+      <div className="shelves">
+
+        <div className="GPUShelf">
+  {randomizedGPUs.map((gpuID, index) => (
+    <div
+      key={index}
+      className="shopItem"
+      onClick={() =>
+        buyComponent(
+          "GPU",
+          gpuID,
+          props.GPU[gpuID].cost
+        )
+      }
+    >
+      <div className="shopTextFormating">
+        <h3 className="shopItemBrand">
+          {props.GPU[gpuID].brand}
+        </h3>
+
+        <p className="shopItemName">
+          {props.GPU[gpuID].name}
+        </p>
+      </div>
+
+      <p className="shopItemCost">
+        ${props.GPU[gpuID].cost}
+      </p>
+
+      <div className="visual" style={{ marginTop: "-100%"}}>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+      </div>
+
+    </div>
+  ))}
+</div>
+<div className="powerSupplyShelf">
+  {randomizedPowerSupplies.map((psuID, index) => (
+    <div
+      key={index}
+      className="shopItem"
+      onClick={() =>
+        buyComponent(
+          "powerSupply",
+          psuID,
+          props.powerSupply[psuID].cost
+        )
+      }
+    >
+      <div className="shopTextFormating">
+
+        <h3 className="shopItemBrand">
+          {props.powerSupply[psuID].brand}
+        </h3>
+
+        <p className="shopItemName">
+          {props.powerSupply[psuID].name}
+        </p>
+
+      </div>
+
+      <p className="shopItemCost">
+        ${props.powerSupply[psuID].cost}
+      </p>
+
+      <div className="visual" style={{ marginTop: "-100%"}}>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+      </div>
+
+    </div>
+  ))}
+</div>
+<div className="motherboardShelf">
+  {randomizedMotherboards.map((mbID, index) => (
+    <div
+      key={index}
+      className="shopItem"
+      onClick={() =>
+        buyComponent(
+          "motherboard",
+          mbID,
+          props.motherboard[mbID].cost
+        )
+      }
+    >
+      <div className="shopTextFormating">
+
+        <h3 className="shopItemBrand">
+          {props.motherboard[mbID].brand}
+        </h3>
+
+        <p className="shopItemName">
+          {props.motherboard[mbID].name}
+        </p>
+
+      </div>
+
+      <p className="shopItemCost">
+        ${props.motherboard[mbID].cost}
+      </p>
+
+      <div className="visual" style={{ marginTop: "-100%"}}>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+      </div>
+
+    </div>
+  ))}
+</div>
+<div className="coolingShelf">
+  {randomizedCoolings.map((coolingID, index) => (
+    <div
+      key={index}
+      className="shopItem"
+      onClick={() =>
+        buyComponent(
+          "cooling",
+          coolingID,
+          props.cooling[coolingID].cost
+        )
+      }
+    >
+      <div className="shopTextFormating">
+
+        <h3 className="shopItemBrand">
+          {props.cooling[coolingID].brand}
+        </h3>
+
+        <p className="shopItemName">
+          {props.cooling[coolingID].name}
+        </p>
+
+      </div>
+
+      <p className="shopItemCost">
+        ${props.cooling[coolingID].cost}
+      </p>
+
+      <div className="visual" style={{ marginTop: "-100%"}}>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+        <div className="effectApplier"></div>
+      </div>
+
+    </div>
+  ))}
+</div>
+      </div>
     </div>
   );
 }
